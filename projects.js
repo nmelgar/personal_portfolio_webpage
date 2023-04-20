ef7c75de7201d0457379e6643655d1aefcb1ed0b
nmelgar <nmelgar16013@gmail.com>
2023-04-20 12:27:05 -0600

    Added: -js to display projects, not displaying projects yet

---
+++ projects.js
@@ -0,0 +1,89 @@
+function generate_projects() {
+  var request = new XMLHttpRequest();
+  request.open(
+    "GET",
+    `https://raw.githubusercontent.com/nmelgar/portfolio/main/projects.json`
+  );
+
+  request.onload = function () {
+    var response = request.response;
+    var parsedData = JSON.parse(response);
+    console.log(parsedData);
+
+    var images_container = document.getElementById("parent_of_giphys");
+
+    for (var i = 0; i < parsedData.data.length; i++) {
+      var col_div_element = document.createElement("div");
+      col_div_element.classList.add("col");
+      var parent_image = document.createElement("div");
+      parent_image.classList.add("card", "shadow-sm");
+
+      var image_url = parsedData.data[i].images.original.url;
+      var image_element = document.createElement("img");
+      image_element.setAttribute("src", image_url);
+      image_element.style.height = "200px";
+      image_element.style.width = "320";
+      // images_container.appendChild(image_element);
+
+      var under_image = document.createElement("div");
+      under_image.classList.add("card-body");
+      var text_under_image = document.createElement("p");
+      text_under_image.classList.add("card-text");
+      var title = parsedData.data[i].title;
+      var short_title = title.slice(0, 15);
+      if (short_title == "") {
+        text_under_image.textContent = "No title available :(";
+      } else {
+        text_under_image.textContent = short_title;
+      }
+      text_under_image.style.fontSize = "10px";
+
+      var div_under_paragraph = document.createElement("div");
+      div_under_paragraph.classList.add(
+        "d-flex",
+        "justify-content-between",
+        "align-items-center"
+      );
+      var button_group = document.createElement("div");
+      button_group.classList.add("btn-group");
+
+      var original_image_link = document.createElement("a");
+      original_image_link.setAttribute("href", image_url);
+      original_image_link.setAttribute("target", "_blank");
+
+      var original_image_button = document.createElement("button");
+      original_image_button.classList.add(
+        "btn",
+        "btn-sm",
+        "btn-outline-secondary"
+      );
+      original_image_button.textContent = "Original Image";
+      original_image_button.setAttribute("type", "button");
+
+      // insert the second button here
+      // var original_image_button = document.createElement('button')
+      // original_image_button.classList.add("btn", "btn-sm", "btn-outline-secondary")
+      // original_image_button.textContent = "Original Image"
+      // original_image_button.setAttribute("type", "button")
+
+      var smallElement = document.createElement("small");
+      smallElement.classList.add("text-body-secondary");
+      smallElement.textContent = word;
+
+      images_container.appendChild(col_div_element);
+      col_div_element.appendChild(parent_image);
+      parent_image.appendChild(image_element);
+      parent_image.appendChild(under_image);
+      under_image.append(text_under_image);
+      under_image.appendChild(div_under_paragraph);
+      div_under_paragraph.appendChild(button_group);
+      button_group.appendChild(original_image_link);
+      original_image_link.appendChild(original_image_button);
+      div_under_paragraph.appendChild(smallElement);
+    }
+  };
+
+  request.send();
+}
+
+generate_projects()
